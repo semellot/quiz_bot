45337db33699586d8d4f7402ed65b9e2772ff44b
Olga <o.panova27@gmail.com>
2023-08-19 11:33:50 +0300

    Change bots

--- tg_bot.py
+++ tg_bot.py
@@ -1,15 +1,9 @@
-import argparse
 import logging
 import os
-import random
+import requests
 
 from dotenv import load_dotenv
-
-from telegram_logger import TelegramLogsHandler
-from questions import get_questions, get_answer
-
 import redis
-
 import telegram
 from telegram import Update, ReplyKeyboardMarkup
 from telegram.ext import (
@@ -18,34 +12,13 @@ from telegram.ext import (
     MessageHandler,
     Filters,
     CallbackContext,
-    CallbackQueryHandler,
     ConversationHandler
 )
 
 
-load_dotenv()
-database_host = os.getenv('REDIS_HOST')
-database_port = os.getenv('REDIS_PORT')
-database_username = os.getenv('REDIS_USERNAME')
-database_password = os.getenv('REDIS_PASSWORD')
-creds_provider = redis.UsernamePasswordCredentialProvider(database_username, database_password)
-database = redis.Redis(
-    host=database_host,
-    port=int(database_port),
-    credential_provider=creds_provider,
-    decode_responses=True
-)
-database.ping()
-
 logger = logging.getLogger('Logger')
 
 ANSWER, QUESTION = range(2)
-
-default_faq_file = 'faq-example.txt'
-parser = argparse.ArgumentParser()
-parser.add_argument('faq_file', help='Файл с вопросами и ответами', nargs='?', default=default_faq_file)
-received_args = parser.parse_args()
-faq_file = received_args.faq_file
 
 
 def start(update: Update, context: CallbackContext) -> None:
@@ -55,22 +28,22 @@ def start(update: Update, context: CallbackContext) -> None:
         ['Мой счёт']
     ]
     reply_markup = ReplyKeyboardMarkup(keyboard)
-    update.message.reply_text('Здравствуйте!', reply_markup=reply_markup)
+    update.message.reply_text('Приветствую! Для участия в квизе нажми «Новый вопрос»! Удачи!', reply_markup=reply_markup)
 
     return QUESTION
 
 
 def handle_new_question_request(update: Update, context: CallbackContext) -> None:
-    question = random.choice(get_questions(faq_file))
-    database.set(update.message.chat_id, question[0])
-    update.message.reply_text(question[0])
+    database = context.bot_data['database']
+    question = database.randomkey()
+    context.user_data['answer'] = database.get(question)
+    update.message.reply_text(question)
 
     return ANSWER
 
 
 def handle_solution_attempt(update: Update, context: CallbackContext) -> None:
-    question = database.get(update.message.chat_id)
-    answer = get_answer(faq_file, question)
+    answer = context.user_data['answer']
     if answer == update.message.text:
         update.message.reply_text('Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»')
         return QUESTION
@@ -80,15 +53,15 @@ def handle_solution_attempt(update: Update, context: CallbackContext) -> None:
 
 
 def handle_surrender(update: Update, context: CallbackContext) -> None:
-    question = database.get(update.message.chat_id)
-    if question:
-        answer = get_answer(faq_file, question)
+    database = context.bot_data['database']
+    answer = context.user_data['answer']
+    if answer:
         update.message.reply_text(f'Правильный ответ: {answer}')
         update.message.reply_text(f'Новый вопрос:')
 
-    question = random.choice(get_questions(faq_file))
-    database.set(update.message.chat_id, question[0])
-    update.message.reply_text(question[0])
+    question = database.randomkey()
+    context.user_data['answer'] = database.get(question)
+    update.message.reply_text(question)
 
     return ANSWER
 
@@ -101,16 +74,32 @@ def cancel(update):
 
 
 def main():
+    load_dotenv()
+    database_host = os.getenv('REDIS_HOST')
+    database_port = os.getenv('REDIS_PORT')
+    database_username = os.getenv('REDIS_USERNAME')
+    database_password = os.getenv('REDIS_PASSWORD')
+    creds_provider = redis.UsernamePasswordCredentialProvider(database_username, database_password)
+    database = redis.Redis(
+        host=database_host,
+        port=int(database_port),
+        credential_provider=creds_provider,
+        decode_responses=True
+    )
+    database.ping()
+
     tg_token = os.getenv('TELEGRAM_TOKEN')
 
     updater = Updater(tg_token, use_context=True)
     dispatcher = updater.dispatcher
+    dispatcher.bot_data['database'] = database
 
     conv_handler = ConversationHandler(
         entry_points=[CommandHandler('start', start)],
 
         states={
             ANSWER: [
+                MessageHandler(Filters.text('Новый вопрос'), handle_new_question_request),
                 MessageHandler(Filters.text('Сдаться'), handle_surrender),
                 MessageHandler(Filters.text, handle_solution_attempt)
             ],
--- vk_bot.py
+++ vk_bot.py
@@ -1,47 +1,19 @@
-import argparse
 import logging
 import os
-import random
 import requests
 
-from telegram_logger import TelegramLogsHandler
-
 from dotenv import load_dotenv
-
-from questions import get_questions, get_answer
-
 import redis
-
 import vk_api as vk
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
 from vk_api.utils import get_random_id
 
 
-load_dotenv()
-database_host = os.getenv('REDIS_HOST')
-database_port = os.getenv('REDIS_PORT')
-database_username = os.getenv('REDIS_USERNAME')
-database_password = os.getenv('REDIS_PASSWORD')
-creds_provider = redis.UsernamePasswordCredentialProvider(database_username, database_password)
-database = redis.Redis(
-    host=database_host,
-    port=int(database_port),
-    credential_provider=creds_provider,
-    decode_responses=True
-)
-database.ping()
-
 logger = logging.getLogger('Logger for vk_bot')
 
-default_faq_file = 'faq-example.txt'
-parser = argparse.ArgumentParser()
-parser.add_argument('faq_file', help='Файл с вопросами и ответами', nargs='?', default=default_faq_file)
-received_args = parser.parse_args()
-faq_file = received_args.faq_file
 
-
-def send_keybord():
+def get_keybord():
     keyboard = VkKeyboard(one_time=True)
     keyboard.add_button('Новый вопрос', color=VkKeyboardColor.PRIMARY)
     keyboard.add_button('Сдаться', color=VkKeyboardColor.NEGATIVE)
@@ -50,67 +22,96 @@ def send_keybord():
     return keyboard.get_keyboard()
 
 
-def handle_new_question_request(event, vk_api):
-    question = random.choice(get_questions(faq_file))
-    database.set(event.user_id, question[0])
+def handle_start(event, vk_api):
     vk_api.messages.send(
         user_id=event.user_id,
         random_id=get_random_id(),
-        keyboard=send_keybord(),
-        message=question[0],
+        keyboard=get_keybord(),
+        message='Приветствую! Для участия в квизе нажми «Новый вопрос»! Удачи!',
     )
 
 
-def handle_solution_attempt(event, vk_api):
-    question = database.get(event.user_id)
-    answer = get_answer(faq_file, question)
-    if answer == event.text:
-        vk_api.messages.send(
-            user_id=event.user_id,
-            random_id=get_random_id(),
-            keyboard=send_keybord(),
-            message='Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»',
-        )
-    else:
-        vk_api.messages.send(
-            user_id=event.user_id,
-            random_id=get_random_id(),
-            keyboard=send_keybord(),
-            message='Неправильно… Попробуешь ещё раз?',
-        )
-
-
-def handle_surrender(event, vk_api):
-    question = database.get(event.user_id)
-    if question:
-        answer = get_answer(faq_file, question)
-        message=f'Правильный ответ: {answer} \n\n Новый вопрос:\n'
-
-    question = random.choice(get_questions(faq_file))
-    database.set(event.user_id, question[0])
+def handle_new_question_request(event, vk_api, database):
+    question = database.randomkey()
+    answer = database.get(question)
     vk_api.messages.send(
         user_id=event.user_id,
         random_id=get_random_id(),
-        keyboard=send_keybord(),
-        message=message+question[0],
+        keyboard=get_keybord(),
+        message=question,
     )
+    return answer
+
+
+def handle_right_solution(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        random_id=get_random_id(),
+        keyboard=get_keybord(),
+        message='Правильно! Поздравляю! Для следующего вопроса нажми «Новый вопрос»',
+    )
+
+
+def handle_wrong_solution(event, vk_api):
+    vk_api.messages.send(
+        user_id=event.user_id,
+        random_id=get_random_id(),
+        keyboard=get_keybord(),
+        message='Неправильно… Попробуешь ещё раз?',
+    )
+
+
+def handle_surrender(event, vk_api, database, answer):
+    message=f'Правильный ответ: {answer} \n\n Новый вопрос:\n'
+    question = database.randomkey()
+    answer = database.get(question)
+    vk_api.messages.send(
+        user_id=event.user_id,
+        random_id=get_random_id(),
+        keyboard=get_keybord(),
+        message=message+question,
+    )
+    return answer
 
 
 def main() -> None:
+    load_dotenv()
+    database_host = os.getenv('REDIS_HOST')
+    database_port = os.getenv('REDIS_PORT')
+    database_username = os.getenv('REDIS_USERNAME')
+    database_password = os.getenv('REDIS_PASSWORD')
+    creds_provider = redis.UsernamePasswordCredentialProvider(database_username, database_password)
+    database = redis.Redis(
+        host=database_host,
+        port=int(database_port),
+        credential_provider=creds_provider,
+        decode_responses=True
+    )
+    database.ping()
+
     vk_group_token = os.getenv('VK_GROUP_TOKEN')
     vk_session = vk.VkApi(token=vk_group_token)
     vk_api = vk_session.get_api()
     longpoll = VkLongPoll(vk_session)
 
     try:
+        answer = ''
         for event in longpoll.listen():
             if event.type == VkEventType.MESSAGE_NEW and event.to_me:
                 if event.text == "Сдаться":
-                    handle_surrender(event, vk_api)
-                if event.text == "Новый вопрос":
-                    handle_new_question_request(event, vk_api)
+                    if answer:
+                        answer = handle_surrender(event, vk_api, database, answer)
+                    else:
+                        answer = handle_new_question_request(event, vk_api, database)
+                elif event.text == "Новый вопрос":
+                    answer = handle_new_question_request(event, vk_api, database)
+                elif answer:
+                    if answer == event.text:
+                        handle_right_solution(event, vk_api)
+                    else:
+                        handle_wrong_solution(event, vk_api)
                 else:
-                    handle_solution_attempt(event, vk_api)
+                    handle_start(event, vk_api)
     except requests.exceptions.HTTPError as err:
         logger.warning(f'Ошибка!\n{err}')
 
